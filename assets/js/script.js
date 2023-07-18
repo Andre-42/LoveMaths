bcc7b3ce7f6c633fcd26e4004dc85386d6c4ea65
Andre-42 <>
2023-07-18 10:11:02 +0000

    added listeners and random num generator to js functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,31 @@
+// Event listener to wait for the html to be loaded
+// Button listeners to activate button reactions
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button")
+    // old syntax: for (let i=0; i<buttons.length; i++){
+    for (let button of buttons) {
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type")==="submit"){
+                alert("You clicked Submit");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+/**
+ * The main game "loop", called when the script is first loaded and after the user's answer has processed
+ */
 function runGame() {
 
+    // create 2 random numbers between 1 and 25
+    let num1 = Math.floor(Math.random()*25)+1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
 }
+
 function checkAnswer() {
 
 }
@@ -23,5 +48,5 @@ function displayMultiplyQuestion() {
 
 }
 function displayDivisionQuestion() {
-    
+
 }
